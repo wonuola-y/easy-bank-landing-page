a363de02ae2cae1a0aedc846ad7726b48fbb65b9
Wonuola <wonuolalonge@gmail.com>
2022-09-07 11:26:38 +0100

    removed border and comments

--- index.js
+++ index.js
@@ -1,24 +1,20 @@
 //  mobile nav bar
-const hamburg = document.querySelector('.hamburger')
-const navList = document.querySelector('.nav-list')
-const overlay = document.querySelector('#overlay')
-const cancel = document.querySelector('.cancel')
-const button = document.querySelectorAll('button')
-const link = document.querySelectorAll('li')
-hamburg.addEventListener('click', ()=>{
-navList.style.display = 'flex';
-overlay.style.display = 'block'
-})
-cancel.addEventListener('click', ()=>{
-    overlay.style.display = 'none'
-
-})
+const hamburg = document.querySelector(".hamburger");
+const navList = document.querySelector(".nav-list");
+const overlay = document.querySelector("#overlay");
+const cancel = document.querySelector(".cancel");
+const button = document.querySelectorAll("button");
+const link = document.querySelectorAll("li");
+hamburg.addEventListener("click", () => {
+  navList.style.display = "flex";
+  overlay.style.display = "block";
+});
+cancel.addEventListener("click", () => {
+  overlay.style.display = "none";
+});
 // for every link clicked
 for (let i = 0; i < button.length; i++) {
-    button[i].addEventListener('click',()=>{
-
-    })
-    
+  button[i].addEventListener("click", () => {});
 }
 
 console.log(hamburg);
