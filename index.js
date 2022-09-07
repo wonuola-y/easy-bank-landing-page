365d43910356de6fe790f5d6158439bd51b2bf5c
Wonuola <wonuolalonge@gmail.com>
2022-09-07 08:37:54 +0100

    footer update

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const hamburg = document.querySelector('.hamburger')
 const navList = document.querySelector('.nav-list')
 const overlay = document.querySelector('#overlay')
 const cancel = document.querySelector('.cancel')
+const button = document.querySelectorAll('button')
+const link = document.querySelectorAll('li')
 hamburg.addEventListener('click', ()=>{
 navList.style.display = 'flex';
 overlay.style.display = 'block'
@@ -11,4 +13,12 @@ cancel.addEventListener('click', ()=>{
     overlay.style.display = 'none'
 
 })
+// for every link clicked
+for (let i = 0; i < button.length; i++) {
+    button[i].addEventListener('click',()=>{
+
+    })
+    
+}
+
 console.log(hamburg);
